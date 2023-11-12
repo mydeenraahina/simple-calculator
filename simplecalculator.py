3154012eb71e2734685e3a67f76d9414e200bba0
MydeenRaahina <145376500+mydeenraahina@users.noreply.github.com>
2023-11-12 01:11:25 -0500

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,17 +1,27 @@
 #simple calculator
-def cal_main():
+def getting_choice():
     def add(a,b):
-        print(f"Answer= {int(a+b)}")
+         print(f"Answer= {a+b}")
     def sub(a,b):
-        print(f"Answer= {int(a-b)}")
+         print(f"Answer= {a-b}")
     def mul(a,b):
-        print(f"Answer= {int(a*b)}")
+        print(f"Answer= {a*b}")
     def div(a,b):
-        print(f"Answer= {int(a/b)}")
+        try:
+            print(f"Answer= {a/b}")
+        except ZeroDivisionError:
+            print("cannot divided the values by zero")
     def mod(a,b):
-        print(f"Answer= {int(a%b)}")
-    frt_num=input("Enter the first number :")
-    sec_num=input("Enter the second number :")
+        try:
+            print(f"Answer= {a%b}")
+        except ZeroDivisionError:
+            print("cannot divided the values by zero")
+    frt_num=eval(input("Enter the first number :"))
+    try:
+        sec_num=eval(input("Enter the second number :"))
+    except NameError:
+        print("Enter numeric values")
+        sec_num=eval(input("Enter the second number :"))
     menu_choice=int(input("Enter you choice to perform calculation :"))
     a,b=frt_num,sec_num
     try:
@@ -29,24 +39,30 @@ def cal_main():
            print("Given value",menu_choice,"is not a valid menu number")
     except ValueError:
         print("value error")
+def menu():
+    print("Menu")
+    print("1.Addition")
+    print("2.subraction")
+    print("3.Multiplication")
+    print("4.Division")
+    print("5.module")
 print("Calculator")
-print("Menu")
-print("1.Addition")
-print("2.subraction")
-print("3.Multiplication")
-print("4.Division")
-print("5.module")
-choice1=input(f"Enter your choice to start calculation [yes \ no]:")
+choice1=input(f"To perform  calculation [yes \ no]:")
 if choice1=="yes":
-    cal_main()
+    menu()
+    getting_choice()
     while True:
         getting_choice_reprefomance=input(f"Do you want to perform another calculation[yes \ no] :")
         if getting_choice_reprefomance=="yes":
-            cal_main()
+            menu()
+            getting_choice()
         else:
             print("exit!")
             break
 else:
     print("Exit!")
       
+      
+
+
       

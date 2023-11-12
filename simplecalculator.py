46ae75ca660c4c85cf55aafe57b9e59463be5ce8
MydeenRaahina <145376500+mydeenraahina@users.noreply.github.com>
2023-11-12 01:48:59 -0500

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -16,12 +16,8 @@ def getting_choice():
             print(f"Answer= {a%b}")
         except ZeroDivisionError:
             print("cannot divided the values by zero")
-    frt_num=eval(input("Enter the first number :"))
-    try:
-        sec_num=eval(input("Enter the second number :"))
-    except NameError:
-        print("Enter numeric values")
-        sec_num=eval(input("Enter the second number :"))
+    frt_num=float(input("Enter the first number :"))
+    sec_num=float(input("Enter the second number :"))
     menu_choice=int(input("Enter you choice to perform calculation :"))
     a,b=frt_num,sec_num
     try:
@@ -40,20 +36,22 @@ def getting_choice():
     except ValueError:
         print("value error")
 def menu():
-    print("Menu")
-    print("1.Addition")
-    print("2.subraction")
-    print("3.Multiplication")
-    print("4.Division")
-    print("5.module")
+    menu_options=["Menu",
+                  "1.Addition",
+                  "2.subraction",
+                  "3.Multiplication",
+                  "4.Division",
+                  "5.modulo"]
+    for option in menu_options:
+        print(str(option))
 print("Calculator")
 choice1=input(f"To perform  calculation [yes \ no]:")
-if choice1=="yes":
+if choice1.upper()=="YES":
     menu()
     getting_choice()
     while True:
         getting_choice_reprefomance=input(f"Do you want to perform another calculation[yes \ no] :")
-        if getting_choice_reprefomance=="yes":
+        if getting_choice_reprefomance.upper()=="YES":
             menu()
             getting_choice()
         else:
@@ -62,7 +60,4 @@ if choice1=="yes":
 else:
     print("Exit!")
       
-      
-
-
       
